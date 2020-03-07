c0ce5f37162b5f5cf1d0cd9ed43432858a0801ff
Alexey Balchunas <bleshik@gmail.com>
2020-03-07 16:19:36 +0700

    do not use $set events, instead read the data directly from DB

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -1,19 +1,23 @@
 package org.example.ecommercerecommendation
 
+import java.sql.{DriverManager, ResultSet}
 import java.util.concurrent.TimeUnit
 
 import org.apache.predictionio.controller.PDataSource
 import org.apache.predictionio.controller.EmptyEvaluationInfo
 import org.apache.predictionio.controller.EmptyActualResult
 import org.apache.predictionio.controller.Params
-import org.apache.predictionio.data.storage.Event
+import org.apache.predictionio.data.storage.{DataMap, EnvironmentFactory, Event, PropertyMap, Storage}
 import org.apache.predictionio.data.store.PEventStore
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import grizzled.slf4j.Logger
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeZone}
+import org.apache.spark.rdd.{JdbcRDD, RDD}
+import org.json4s.JObject
+import org.json4s.native.Serialization
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, TimeUnit}
 
 case class DataSourceParams(appName: String, targetEntityTypes: Set[String], eventNames: Set[String]) extends Params
 
@@ -22,16 +26,14 @@ class DataSource(val dsp: DataSourceParams)
       EmptyEvaluationInfo, Queries, EmptyActualResult] {
 
   @transient lazy val logger = Logger[this.type]
+  @transient private implicit lazy val formats = org.json4s.DefaultFormats
 
   override
   def readTraining(sc: SparkContext): TrainingData = {
 
     logger.info("Started loading users")
     // create a RDD of (entityID, User)
-    val usersRDD: RDD[(String, User)] = PEventStore.aggregateProperties(
-      appName = dsp.appName,
-      entityType = "user"
-    )(sc).map { case (entityId, properties) =>
+    val usersRDD: RDD[(String, User)] = propertiesRdd(sc, "user").map { case (entityId, properties) =>
       val user = try {
         User(role = properties.getOpt[String]("role").orElse(Some("user")))
       } catch {
@@ -48,27 +50,8 @@ class DataSource(val dsp: DataSourceParams)
 
     // create a RDD of (entityID, Item)
     val itemsRDD: Map[String, RDD[(String, Item)]] = dsp.targetEntityTypes.map { entityType =>
-      (entityType, PEventStore.aggregateProperties(
-          appName = dsp.appName,
-          entityType = entityType
-        )(sc).map { case (entityId, properties) =>
-          val item = try {
-            Item(
-              imageExists = properties.getOrElse[Boolean]("imageExists", false),
-              categories = properties.getOpt[List[String]]("categories"),
-              status = properties.getOpt[String]("status"),
-              lastUpdated = properties.lastUpdated,
-              reward = properties.getOpt[Double]("reward").getOrElse(0.0)
-            )
-          } catch {
-            case e: Exception => {
-              logger.error(s"Failed to get properties ${properties} of" +
-                s" item ${entityId}. Exception: ${e}.")
-              throw e
-            }
-          }
-          (entityId, item)
-        }.cache())
+      println(entityType + " " + itemsRdd(sc, entityType).count())
+      (entityType, itemsRdd(sc, entityType))
     }.toMap
     logger.info("Loaded items")
     logger.info("Started loading events")
@@ -85,6 +68,74 @@ class DataSource(val dsp: DataSourceParams)
       items = itemsRDD,
       events = eventsRDD
     )
+  }
+
+  private def propertiesRdd(sc: SparkContext, entityType: String): RDD[(String, PropertyMap)] = {
+    val q = if (entityType == "product") {
+      s"""
+        select
+          'product/' || id as id, '{"categories":[' || (SELECT STRING_AGG('"' || category.id || '"', ',') FROM category JOIN product_category_relation AS pcr ON pcr.category_id = category.id WHERE product.id = pcr.product_id) || '],"status":"' || published_status || '","language":"english","location":"USA","reward":' || product.fee || '}' as properties,
+          GREATEST(updated_at, created_at) as updated_at
+        from product
+        where published_status = 'published' and id >= ? and id < ?
+      """.replace("\n", " ")
+    } else if (entityType == "user") {
+      s"""
+        select
+          'user/' || id as id, '{"role":"' || role || '"}' as properties, GREATEST(updated_at, created_at) as updated_at
+        from "user"
+        where id >= ? and id < ?
+      """.replace("\n", " ")
+    } else {
+      s"""
+        select
+          'shop/' || id as id, '{"categories":[' || (SELECT STRING_AGG('"' || category.id || '"', ',') FROM category JOIN product_category_relation AS pcr ON pcr.category_id = category.id JOIN product AS product ON pcr.product_id=product.id WHERE product.shop_id = shop.id) || '],"status":"' || status || '","language":"english","location":"USA","reward":' || shop.fee || '}' as properties,
+          GREATEST(updated_at, created_at) as updated_at
+        from shop
+        where status in ('enabled', 'disconnected') and id >= ? and id < ?
+      """.replace("\n", " ")
+    }
+    val es = Storage.getConfig("PGSQL").get
+    val username = es.properties("USERNAME")
+    val password = es.properties("PASSWORD")
+    val url = es.properties("URL")
+    val lower = 0
+    val upper = Long.MaxValue
+    new JdbcRDD(sc, () => { DriverManager.getConnection(url, username, password) }, q, lower, upper, 1,
+      (r: ResultSet) => {
+        Event(
+          eventId = Option(r.getString("id")),
+          event = "$set",
+          entityType = entityType,
+          entityId = r.getString("id"),
+          properties = Option(r.getString("properties")).map(x =>
+            DataMap(Serialization.read[JObject](x))).getOrElse(DataMap()),
+          eventTime = new DateTime(r.getTimestamp("updated_at").getTime, DateTimeZone.forID("UTC"))
+        )
+      }).map { e =>
+      (e.entityId, PropertyMap(e.properties.fields, e.eventTime, e.eventTime))
+    }
+  }
+
+  private def itemsRdd(sc: SparkContext, entityType: String): RDD[(String, Item)] = {
+    propertiesRdd(sc, entityType).map { case (entityId, properties) =>
+      val item = try {
+        Item(
+          imageExists = properties.getOrElse[Boolean]("imageExists", false),
+          categories = properties.getOpt[List[String]]("categories"),
+          status = properties.getOpt[String]("status"),
+          lastUpdated = properties.lastUpdated,
+          reward = properties.getOpt[Double]("reward").getOrElse(0.0)
+        )
+      } catch {
+        case e: Exception => {
+          logger.error(s"Failed to get properties ${properties} of" +
+            s" item ${entityId}. Exception: ${e}.")
+          throw e
+        }
+      }
+      (entityId, item)
+    }.cache()
   }
 }
 
